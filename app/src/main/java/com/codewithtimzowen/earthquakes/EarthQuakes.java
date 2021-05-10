1f63f1686aea996dbce399e2e63a2ad61a524261
TimzOwen <ktimothy@kabarak.ac.ke>
2021-05-10 15:53:32 +0300

    remoter setters

--- app/src/main/java/com/codewithtimzowen/earthquakes/EarthQuakes.java
+++ app/src/main/java/com/codewithtimzowen/earthquakes/EarthQuakes.java
@@ -12,25 +12,19 @@ public class EarthQuakes {
         this.mDate = date;
     }
 
-    public void setmDate(String mDate) {
-        this.mDate = mDate;
-    }
+
 
     public String getmDate() {
         return mDate;
     }
 
-    public void setmLocation(String mLocation) {
-        this.mLocation = mLocation;
-    }
+
 
     public String getmLocation() {
         return mLocation;
     }
 
-    public void setmMagnitude(String mMagnitude) {
-        this.mMagnitude = mMagnitude;
-    }
+
 
     public String getmMagnitude() {
         return mMagnitude;
