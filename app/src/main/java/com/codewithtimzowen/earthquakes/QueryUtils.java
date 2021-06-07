6b9448919b1f828ad8d3254c3114491b1a027732
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-07 12:37:45 +0300

    bug fix , service failed place -> location

--- app/src/main/java/com/codewithtimzowen/earthquakes/QueryUtils.java
+++ app/src/main/java/com/codewithtimzowen/earthquakes/QueryUtils.java
@@ -7,9 +7,9 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-/**
- * Helper methods related to requesting and receiving earthquake data from USGS.
- */
+
+ // Helper methods related to requesting and receiving earthquake data from USGS.
+
 public final class QueryUtils {
 
     /** Sample JSON response for a USGS query */
@@ -24,11 +24,7 @@ public final class QueryUtils {
             "{\"type\":\"Feature\",\"properties\":{\"mag\":6.5,\"place\":\"227km SE of Sarangani, Philippines\",\"time\":1452530285900,\"updated\":1459304874040,\"tz\":480,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us10004dj5\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us10004dj5&format=geojson\",\"felt\":1,\"cdi\":2.7,\"mmi\":7.5,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":650,\"net\":\"us\",\"code\":\"10004dj5\",\"ids\":\",at00o0srjp,pt16011050,us10004dj5,gcmt20160111163807,\",\"sources\":\",at,pt,us,gcmt,\",\"types\":\",cap,dyfi,geoserve,impact-link,impact-text,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,tectonic-summary,\",\"nst\":null,\"dmin\":3.144,\"rms\":0.72,\"gap\":22,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.5 - 227km SE of Sarangani, Philippines\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[126.8621,3.8965,13]},\"id\":\"us10004dj5\"},\n" +
             "{\"type\":\"Feature\",\"properties\":{\"mag\":6,\"place\":\"Pacific-Antarctic Ridge\",\"time\":1451986454620,\"updated\":1459202978040,\"tz\":-540,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us10004bgk\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us10004bgk&format=geojson\",\"felt\":0,\"cdi\":1,\"mmi\":0,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":0,\"sig\":554,\"net\":\"us\",\"code\":\"10004bgk\",\"ids\":\",us10004bgk,gcmt20160105093415,\",\"sources\":\",us,gcmt,\",\"types\":\",cap,dyfi,geoserve,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":30.75,\"rms\":0.67,\"gap\":71,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.0 - Pacific-Antarctic Ridge\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-136.2603,-54.2906,10]},\"id\":\"us10004bgk\"}],\"bbox\":[-153.4051,-54.2906,10,158.5463,59.6363,582.56]}";
 
-    /**
-     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
-     * This class is only meant to hold static variables and methods, which can be accessed
-     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
-     */
+  //private constructor
     private QueryUtils() {
     }
 
@@ -41,7 +37,6 @@ public final class QueryUtils {
         // Create an empty ArrayList that we can start adding earthquakes to
         ArrayList<EarthQuakes> earthquakes = new ArrayList<>();
 
-
         try {
 
             // create a Json Object response
@@ -52,13 +47,16 @@ public final class QueryUtils {
 
             //Loop through all elements in the array
             for (int i = 0; i < earthquakeArray.length(); i ++){
+
                 //Get JSON Object at the specified index
                 JSONObject currentEarthquake = earthquakeArray.getJSONObject(i);
+
                 //Get the JSON Object with JSON properties
                 JSONObject properties = currentEarthquake.getJSONObject("properties");
+
                 // Find individual Strings
                 String magnitude = properties.getString("mag");
-                String location = properties.getString("location");
+                String location = properties.getString("place");
                 String time = properties.getString("time");
 
                 //create a new object from the three strings
@@ -67,7 +65,7 @@ public final class QueryUtils {
             }
 
         } catch (JSONException e) {
-           .
+
             Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
         }
 
