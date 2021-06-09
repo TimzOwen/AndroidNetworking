80c1499f17197dde49ef1e0cd5c4f9be0ea37e8f
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-09 20:49:30 +0300

    started the Asyc task to fetch the data

--- app/src/main/java/com/codewithtimzowen/earthquakes/MainActivity.java
+++ app/src/main/java/com/codewithtimzowen/earthquakes/MainActivity.java
@@ -43,13 +43,23 @@ public class MainActivity extends AppCompatActivity {
             startActivity(websiteIntent);
 
         });
+
+        // Start the AsyncTask to fetch the earthquake data
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
         
     }
     private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<EarthQuakes>> {
 
         @Override
         protected List<EarthQuakes> doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
 
+            List<EarthQuakes> result = QueryUtils.fetchEarthquakeData(urls[0]);
+            return result;
         }
 
         @Override
